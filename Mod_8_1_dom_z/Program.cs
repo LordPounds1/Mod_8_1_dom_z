0d1b6c426f00b5e5cf31be0994c2ba719f6cf48a
LordPounds1 <91436904+LordPounds1@users.noreply.github.com>
2023-10-25 22:08:54 +0600

    Add project files.

---
+++ Mod_8_1_dom_z/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Mod_8_1_dom_z
+{
+    class Arr
+    {
+        int[] array;
+
+        public Arr(int length)
+        {
+            array = new int[length];
+            Random r = new Random();
+            for (int i = 0; i < array.Length; i++)
+                array[i] = r.Next(-5, 6); // От -5 до 5
+        }
+
+        public int Length
+        {
+            get { return array.Length; }
+        }
+
+        public void Output()
+        {
+            foreach (int item in array)
+                Console.Write(item + " ");
+            Console.WriteLine();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Arr a1 = new Arr(10);
+            Arr a2 = new Arr(20);
+            Console.WriteLine("В массиве 1 " + a1.Length + " элементов.");
+            Console.WriteLine("Массив 1:");
+            a1.Output();
+            Console.WriteLine("В массиве 2 " + a2.Length + " элементов.");
+            Console.WriteLine("Массив 2:");
+            a2.Output();
+            Console.ReadLine();
+        }
+    }
+}
